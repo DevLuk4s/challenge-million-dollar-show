6780a47081036fd670926b1a8ad954a87ccb5df1
Lucas Gabriel <114165245+DevLuk4s@users.noreply.github.com>
2023-12-17 21:59:57 -0300

    update

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,5 +1,21 @@
+//CSS
+import "./App.css";
+
+//Components
+import Home from "./components/Home";
+
+//Hooks
+import { useCallback, useEffct, useState } from "react";
+
+//Dados
+import { data } from "./data/words";
+
 function App() {
-  return <></>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ my-project/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <h1 className="tittle">Million Dollar Show</h1>
+      <p className="tittle-p">Clique aqui abaixo pra joga agora</p>
+      <button className="button">Começar Jogo Agora</button>
+    </>
+  );
+}
+
+export default Home;
